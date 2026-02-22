6d312ac190a12291fd8774e4d4a84dd92215f973
Neil Chen <neilchen1998@gmail.com>
2026-02-22 09:18:41 -0600

    set up Sudoku repo

---
+++ apps/app.cc
@@ -0,0 +1,10 @@
+#include <stdlib.h> // EXIT_SUCCESS, EXIT_FAILURE
+
+#include <fmt/core.h>   // fmt::print
+
+int main(int argc, char* argv[])
+{
+    fmt::println("Hello, world!");
+
+    return EXIT_SUCCESS;
+}
