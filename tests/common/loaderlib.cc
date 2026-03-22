6375c50afa51dac5203b63d2a6cc09d6906169f3
Neil Chen <90213006+neilchen1998@users.noreply.github.com>
2026-03-22 09:46:47 -0500

    Import Puzzles for Benchmark from JSON (#18)
    * created loaderlib for all json import related code
    
    * added TEST_DATA_DIR macro
    
    * loaded puzzles from json
    
    * refactored the test bench structure
    
    * updated the benchmark table in README.md

---
+++ tests/common/loaderlib.cc
@@ -0,0 +1,40 @@
+#include <filesystem>   // std::filesystem
+#include <fstream>      // std::ifstream
+#include <vector>    // std::vector
+
+#include <nlohmann/json.hpp>    // nlohmann::json
+
+#include "loaderlib.hpp"
+
+namespace fs = std::filesystem;
+
+/// @brief Gets the directory of the test data
+/// @return Test data directory
+std::string GetTestDataPath()
+{
+    return TEST_DATA_DIR;
+}
+
+/// @brief Load all the test cases from the data directory
+/// @param dir The directory of the data
+/// @return A vector of SumokuTestData
+std::vector<SumokuTestData> LoadAllPuzzles(std::string_view dir)
+{
+    std::vector<SumokuTestData> testCases;
+
+    // Iterate over all the json entries in the directory
+    for (const auto& entry : fs::directory_iterator(dir))
+    {
+        if (entry.path().extension() == ".json")
+        {
+            std::ifstream file{entry.path()};
+            nlohmann::json j;
+            file >> j;
+
+            SumokuTestData puzzle = j.get<SumokuTestData>();
+            testCases.push_back(puzzle);
+        }
+    }
+
+    return testCases;
+}
