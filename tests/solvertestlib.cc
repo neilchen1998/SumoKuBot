472d9121c2f1cf9779b10dba3a20560d37943280
Neil Chen <neilchen1998@gmail.com>
2026-06-28 09:27:14 -0500

    changed solver::SudokuSolver to take int and added Sudoku Solvers TEMPLATE_TEST_CASE

--- tests/solvertestlib.cc
+++ tests/solvertestlib.cc
@@ -3,19 +3,17 @@
 #include <algorithm>  // std::ranges::all_of
 #include <cmath> // std::sqrt
 #include <concepts> // concept
-#include <filesystem>   // std::filesystem
-#include <fstream>      // std::ifstream
 #include <numeric>  // std::iota
-#include <ranges>    // std::ranges::input_range
-#include <unordered_set>    // std::unordered_set
 #include <vector>    // std::vector
 
+#include <catch2/catch_template_test_macros.hpp>    // TestType
 #include <catch2/catch_test_macros.hpp> // TEST_CASE, SECTION, REQUIRE
-#include <catch2/matchers/catch_matchers_all.hpp>   // Catch::Matchers::Equals
 #include <catch2/generators/catch_generators_all.hpp>   // Catch::Generators::values
+#include <catch2/matchers/catch_matchers_all.hpp>   // Catch::Matchers::Equals
 
-#include "solver/solverlib.hpp" // solver::SumokuMRV
+#include "board/boardlib.hpp"   // BoardType
 #include "loader/loaderlib.hpp" // GetTestDataPath
+#include "solver/solverlib.hpp" // solver::SumokuMRV
 
 /// @brief Converts a character digit or integer to an int
 /// @tparam T Must be char or int
@@ -136,23 +134,52 @@ void validate_sukodu_row_column_box_constraints(const std::vector<std::vector<T>
     }
 }
 
-TEST_CASE( "Sudoku", "[main]" )
-{
-    SECTION("Puzzle 0", "[trivial case]")
-    {
-        std::vector<std::vector<char>> board ({{'5','3','.','.','7','.','.','.','.'},{'6','.','.','1','9','5','.','.','.'},{'.','9','8','.','.','.','.','6','.'},{'8','.','.','.','6','.','.','.','3'},{'4','.','.','8','.','3','.','.','1'},{'7','.','.','.','2','.','.','.','6'},{'.','6','.','.','.','.','2','8','.'},{'.','.','.','4','1','9','.','.','5'},{'.','.','.','.','8','.','.','7','9'}});
-        std::vector<std::vector<char>> ans ({{'5','3','4','6','7','8','9','1','2'},{'6','7','2','1','9','5','3','4', '8'},{'1','9','8','3','4','2','5','6','7'},{'8','5','9','7','6','1','4','2','3'},{'4','2','6','8','5','3','7','9','1'},{'7','1','3','9','2','4','8','5','6'},{'9','6','1','5','3','7','2','8','4'},{'2','8','7','4','1','9','6','3','5'},{'3','4','5','2','8','6','1','7','9'}});
+std::vector<std::vector<int>> board0({
+    {5, 3, 0, 0, 7, 0, 0, 0, 0},
+    {6, 0, 0, 1, 9, 5, 0, 0, 0},
+    {0, 9, 8, 0, 0, 0, 0, 6, 0},
+    {8, 0, 0, 0, 6, 0, 0, 0, 3},
+    {4, 0, 0, 8, 0, 3, 0, 0, 1},
+    {7, 0, 0, 0, 2, 0, 0, 0, 6},
+    {0, 6, 0, 0, 0, 0, 2, 8, 0},
+    {0, 0, 0, 4, 1, 9, 0, 0, 5},
+    {0, 0, 0, 0, 8, 0, 0, 7, 9}
+});
 
-        solver::SudokuSolver s {board};
+std::vector<std::vector<int>> ans0({
+    {5, 3, 4, 6, 7, 8, 9, 1, 2},
+    {6, 7, 2, 1, 9, 5, 3, 4, 8},
+    {1, 9, 8, 3, 4, 2, 5, 6, 7},
+    {8, 5, 9, 7, 6, 1, 4, 2, 3},
+    {4, 2, 6, 8, 5, 3, 7, 9, 1},
+    {7, 1, 3, 9, 2, 4, 8, 5, 6},
+    {9, 6, 1, 5, 3, 7, 2, 8, 4},
+    {2, 8, 7, 4, 1, 9, 6, 3, 5},
+    {3, 4, 5, 2, 8, 6, 1, 7, 9}
+});
+
+struct SudokuTest
+{
+    std::vector<std::vector<int>> board;
+    std::vector<std::vector<int>> ans;
+};
+
+std::vector<SudokuTest> sudokuTests {{board0, ans0}};
+
+TEMPLATE_TEST_CASE( "Sudoku Solvers", "[solver]", solver::SudokuSolver )
+{
+    for (auto& test : sudokuTests)
+    {
+        TestType s {test.board};
 
         s.Solve();
 
         auto ret = s.GetSolution();
         REQUIRE (ret != std::nullopt);
 
-        std::vector<std::vector<char>> solution = *ret;
+        std::vector<std::vector<int>> solution = *ret;
 
-        REQUIRE (solution.size() == ans.size());
+        REQUIRE (solution.size() == test.ans.size());
         validate_boad_is_square(solution);
         validate_sukodu_row_column_box_constraints(solution);
     }
